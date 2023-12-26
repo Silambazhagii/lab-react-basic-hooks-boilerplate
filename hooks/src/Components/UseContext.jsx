1cd5bda8e1444d40bd7a37162909f0ce228a419b
Silambazhagii <silambazhagii@gmail.com>
2023-12-26 18:24:17 +0530

    updated

--- hooks/src/Components/UseContext.jsx
+++ hooks/src/Components/UseContext.jsx
@@ -22,7 +22,7 @@ function UseContext() {
 
   const paragraph = () => {
     return (
-      <>  
+      < >  
         He hid under the covers hoping that nobody would notice him there. It really didn't make much sense since it would be obvious to anyone who walked into the room there was someone hiding there, but there was someone hiding there, but he still held out hope. He heard footsteps coming down the hall and stop in front of the bedroom door. He heard the squeak of the door hinges and someone opened the bedroom door. He held his breath waiting for whoever was about to discover him, but they never did.
       </>
     );
@@ -33,9 +33,9 @@ function UseContext() {
       <div style={themeStyle} className="box">
         This is made by Kalvium
         <br />
-        <button onClick={toggleParagraph}>Content</button>
-        <br />
         {showParagraph && paragraph()}
+        <br />
+        <button onClick={toggleParagraph}>Content</button>
         <div className="count">{count}</div>
         <button onClick={() => setCount(count + 1)}>Like</button>
       </div>
